516c3df2e8be58bff1ffc3132361718f641a74d2
MelvinCou <113338357+MelvinCou@users.noreply.github.com>
2024-03-04 15:55:21 +0000

    docs: Format comments

--- server/main.go
+++ server/main.go
@@ -6,33 +6,28 @@ import (
 	"github.com/gin-gonic/gin"
 	ginSwagger "github.com/swaggo/gin-swagger"
 
-	//docs "server/docs"
+	docs "server/docs"
 
 	swaggerFiles "github.com/swaggo/files"
 )
 
-// @title           Idee API
-// @version         1.0
-// @description     This is the API of the Idee project!
-// @termsOfService  http://swagger.io/terms/
-
-// @contact.name   API Support
-// @contact.url    http://www.swagger.io/support
-// @contact.email  support@swagger.io
-
-// @license.name  Apache 2.0
-// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host      localhost:8080
-// @BasePath  /api
-
-// @securityDefinitions.basic  BasicAuth
-
-// @externalDocs.description  OpenAPI
-// @externalDocs.url          https://swagger.io/resources/open-api/
-// @schemes http https
+//	@title						Idee API
+//	@version					1.0
+//	@description				This is the API of the Idee project!
+//	@termsOfService				http://swagger.io/terms/
+//	@contact.name				API Support
+//	@contact.url				http://www.swagger.io/support
+//	@contact.email				support@swagger.io
+//	@license.name				Apache 2.0
+//	@license.url				http://www.apache.org/licenses/LICENSE-2.0.html
+//	@host						localhost:8080
+//	@BasePath					/api
+//	@securityDefinitions.basic	BasicAuth
+//	@externalDocs.description	OpenAPI
+//	@externalDocs.url			https://swagger.io/resources/open-api/
 func main() {
 	r := gin.Default()
+	docs.SwaggerInfo.BasePath = "/"
 
 	api := r.Group("/api")
 	{
@@ -44,17 +39,17 @@ func main() {
 }
 
 // Ping godoc
-// @Summary      Ping
-// @Description  Ping the server
-// @Tags         ping
-// @Accept       json
-// @Produce      json
-// @Param        id   path      int  true  "Account ID"
-// @Success      200  {object}  Response
-// @Failure      400  {string}	string    "Bad Request"
-// @Failure      404  {string}	string    "Not Found"
-// @Failure      500  {string}	string    "Internal Server Error"
-// @Router       /ping [get]
+//
+//	@Summary		Ping
+//	@Description	Ping the server
+//	@Tags			ping
+//	@Accept			json
+//	@Produce		json
+//	@Success		200	{object}	Response
+//	@Failure		400	{string}	string	"Bad Request"
+//	@Failure		404	{string}	string	"Not Found"
+//	@Failure		500	{string}	string	"Internal Server Error"
+//	@Router			/ping [get]
 func Ping(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, Response{Message: "pong"})
 }
