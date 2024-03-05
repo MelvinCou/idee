31c8a78f8f2f998951e98682efcbb7186c6a07ea
MansourWolou <woloumansour@gmail.com>
2024-03-05 16:34:13 +0000

    fix(.github/workflowx): fix swagger step
    Need to init swagger before formating
    
    Linked #6

--- server/main.go
+++ server/main.go
@@ -11,20 +11,20 @@ import (
 	swaggerFiles "github.com/swaggo/files"
 )
 
-// @title						Idee API
-// @version					1.0
-// @description				This is the API of the Idee project!
-// @termsOfService				http://swagger.io/terms/
-// @contact.name				API Support
-// @contact.url				http://www.swagger.io/support
-// @contact.email				support@swagger.io
-// @license.name				Apache 2.0
-// @license.url				http://www.apache.org/licenses/LICENSE-2.0.html
-// @host						localhost:8080
-// @BasePath					/api
-// @securityDefinitions.basic	BasicAuth
-// @externalDocs.description	OpenAPI
-// @externalDocs.url			https://swagger.io/resources/open-api/
+//	@title						Idee API
+//	@version					1.0
+//	@description				This is the API of the Idee project!
+//	@termsOfService				http://swagger.io/terms/
+//	@contact.name				API Support
+//	@contact.url				http://www.swagger.io/support
+//	@contact.email				support@swagger.io
+//	@license.name				Apache 2.0
+//	@license.url				http://www.apache.org/licenses/LICENSE-2.0.html
+//	@host						localhost:8080
+//	@BasePath					/api
+//	@securityDefinitions.basic	BasicAuth
+//	@externalDocs.description	OpenAPI
+//	@externalDocs.url			https://swagger.io/resources/open-api/
 func main() {
 	r := gin.Default()
 	docs.SwaggerInfo.BasePath = "/"
