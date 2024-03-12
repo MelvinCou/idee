7b34b113787f52062ce69fb719fb8a9f6dedd5a1
MelvinCou <melvin.courjaud@epitech.eu>
2024-03-12 13:32:09 +0000

    feature(front): Add files generated by `swagger-typescript-api`

---
+++ front/src/api/Ping.ts
@@ -0,0 +1,32 @@
+/* eslint-disable */
+/* tslint:disable */
+/*
+ * ---------------------------------------------------------------
+ * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
+ * ##                                                           ##
+ * ## AUTHOR: acacode                                           ##
+ * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
+ * ---------------------------------------------------------------
+ */
+
+import { type MainResponse } from "./data-contracts";
+import { ContentType, HttpClient, type RequestParams } from "./http-client";
+
+export class Ping<SecurityDataType = unknown> extends HttpClient<SecurityDataType> {
+  /**
+   * @description Ping the server
+   *
+   * @tags ping
+   * @name PingList
+   * @summary Ping
+   * @request GET:/ping
+   */
+  pingList = (params: RequestParams = {}) =>
+    this.request<MainResponse, string>({
+      path: `/ping`,
+      method: "GET",
+      type: ContentType.Json,
+      format: "json",
+      ...params,
+    });
+}
