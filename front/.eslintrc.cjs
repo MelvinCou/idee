2c376b4c01e805333355b2d1ad53d9585bc48fa5
Nokomi6 <damien.legrand@epitech.eu>
2024-03-12 16:24:54 +0000

    ci: apply format and lint in front

--- front/.eslintrc.cjs
+++ front/.eslintrc.cjs
@@ -7,63 +7,58 @@ module.exports = {
   },
   root: true,
   extends: [
-    'prettier',
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    '@vue/eslint-config-prettier/skip-formatting',
+    "prettier",
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "@vue/eslint-config-prettier/skip-formatting",
   ],
   overrides: [
     {
-      files: [
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-        'cypress/support/**/*.{js,ts,jsx,tsx}',
-      ],
-      extends: [
-        'plugin:cypress/recommended',
-      ],
+      files: ["cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}", "cypress/support/**/*.{js,ts,jsx,tsx}"],
+      extends: ["plugin:cypress/recommended"],
     },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
   },
-  plugins: ['vue', '@typescript-eslint', 'prettier', 'import', 'jsx-a11y', 'sort-keys-fix'],
+  plugins: ["vue", "@typescript-eslint", "prettier", "import", "jsx-a11y", "sort-keys-fix"],
   settings: {
-    'import/resolver': {
-      'node': {
-        'moduleDirectory': ['node_modules', '.'],
+    "import/resolver": {
+      node: {
+        moduleDirectory: ["node_modules", "."],
       },
     },
   },
   rules: {
-    'prettier/prettier': 'error',
-    'no-console': 'warn',
-    'no-debugger': 'warn',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    "prettier/prettier": "error",
+    "no-console": "warn",
+    "no-debugger": "warn",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
 
     // TypeScript's `noFallthroughCasesInSwitch` option is more robust (#6906)
-    'default-case': 'off',
+    "default-case": "off",
     // "tsc" already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/291)
-    'no-dupe-class-members': 'off',
+    "no-dupe-class-members": "off",
     // "tsc" already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/477)
-    'no-undef': 'off',
+    "no-undef": "off",
     // "tsc" already handles this
-    'import/named': 'off',
+    "import/named": "off",
     // Add TypeScript specific rules (and turn off ESLint equivalents)
-    '@typescript-eslint/consistent-type-assertions': 'warn',
-    'no-array-constructor': 'off',
-    '@typescript-eslint/no-array-constructor': 'warn',
-    'no-redeclare': 'off',
-    '@typescript-eslint/no-redeclare': 'warn',
-    'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': [
-      'warn',
+    "@typescript-eslint/consistent-type-assertions": "warn",
+    "no-array-constructor": "off",
+    "@typescript-eslint/no-array-constructor": "warn",
+    "no-redeclare": "off",
+    "@typescript-eslint/no-redeclare": "warn",
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": [
+      "warn",
       {
         functions: false,
         classes: false,
@@ -71,16 +66,17 @@ module.exports = {
         typedefs: false,
       },
     ],
-    'no-unused-expressions': 'off',
-    '@typescript-eslint/no-unused-expressions': [
-      'error',
+    "no-unused-expressions": "off",
+    "@typescript-eslint/no-unused-expressions": [
+      "error",
       {
         allowShortCircuit: true,
         allowTernary: true,
         allowTaggedTemplates: true,
       },
     ],
-    'no-useless-constructor': 'off',
-    '@typescript-eslint/no-useless-constructor': 'warn',
+    "no-useless-constructor": "off",
+    "@typescript-eslint/no-useless-constructor": "warn",
+    '@typescript-eslint/consistent-type-imports': 'error',
   },
 };
