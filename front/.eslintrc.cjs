ec323d125711bd1f0c7bd07702186556ffd4da50
Nokomi6 <damien.legrand@epitech.eu>
2024-03-12 15:22:30 +0000

    ci: add rules for eslint and more params for prettier

--- front/.eslintrc.cjs
+++ front/.eslintrc.cjs
@@ -1,26 +1,86 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
   root: true,
-  'extends': [
+  extends: [
+    'prettier',
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
   overrides: [
     {
       files: [
         'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-        'cypress/support/**/*.{js,ts,jsx,tsx}'
+        'cypress/support/**/*.{js,ts,jsx,tsx}',
       ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
-    }
+      extends: [
+        'plugin:cypress/recommended',
+      ],
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint', 'prettier', 'import', 'jsx-a11y', 'sort-keys-fix'],
+  settings: {
+    'import/resolver': {
+      'node': {
+        'moduleDirectory': ['node_modules', '.'],
+      },
+    },
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+
+    // TypeScript's `noFallthroughCasesInSwitch` option is more robust (#6906)
+    'default-case': 'off',
+    // "tsc" already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/291)
+    'no-dupe-class-members': 'off',
+    // "tsc" already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/477)
+    'no-undef': 'off',
+    // "tsc" already handles this
+    'import/named': 'off',
+    // Add TypeScript specific rules (and turn off ESLint equivalents)
+    '@typescript-eslint/consistent-type-assertions': 'warn',
+    'no-array-constructor': 'off',
+    '@typescript-eslint/no-array-constructor': 'warn',
+    'no-redeclare': 'off',
+    '@typescript-eslint/no-redeclare': 'warn',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': [
+      'warn',
+      {
+        functions: false,
+        classes: false,
+        variables: false,
+        typedefs: false,
+      },
+    ],
+    'no-unused-expressions': 'off',
+    '@typescript-eslint/no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+        allowTaggedTemplates: true,
+      },
+    ],
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-useless-constructor': 'warn',
+  },
+};
