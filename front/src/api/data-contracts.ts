e54db1345ebd702bb8ebb33208aa875f2d17f39e
MelvinCou <melvin.courjaud@epitech.eu>
2024-03-12 13:32:09 +0000

    feature(front): Add files generated by `swagger-typescript-api`

---
+++ front/src/api/data-contracts.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+/* tslint:disable */
+/*
+ * ---------------------------------------------------------------
+ * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
+ * ##                                                           ##
+ * ## AUTHOR: acacode                                           ##
+ * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
+ * ---------------------------------------------------------------
+ */
+
+export interface MainResponse {
+  /** @example "pong" */
+  message?: string;
+}
