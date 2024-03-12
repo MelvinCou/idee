e8fd7e689ecd97cb69232ee922d2874207a61071
MansourWolou <woloumansour@gmail.com>
2024-03-12 13:42:58 +0000

    feat(front): Using tailwind and vuetify
    I think that the basics vuejs components are useless , but i'm gonna wait the review before deleting them
    The intellisense seems to have an issue on App.vue line 2: the vue component load on the page but it is not found on the file
    
    Linked #31

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,29 +1,13 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import MainLayout from './views/MainLayout.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <h1 class="text-3xl font-bold underline text-amber-950 bg-red-700">Hello world!</h1>
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <MainLayout></MainLayout>
 </template>
 
 <style scoped>
-header {
+/* header {
   line-height: 1.5;
   max-height: 100vh;
 }
@@ -83,5 +67,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-}
+} */
 </style>
