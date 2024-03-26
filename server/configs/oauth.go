9c18d4baf4759b596141d127d8982121e67de401
Legrand Damien <74177609+Nokomi6@users.noreply.github.com>
2024-03-26 23:35:48 +0000

    docs: add some comments in oauth files

--- server/configs/oauth.go
+++ server/configs/oauth.go
@@ -1,33 +1,31 @@
 package configs
 
 import (
-	"log"
-	"os"
+    "log"
+    "os"
 
-	"github.com/joho/godotenv"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/github"
+    "github.com/joho/godotenv"
+    "golang.org/x/oauth2"
+    "golang.org/x/oauth2/github"
 )
 
-type Config struct {
-	GithubLoginConfig oauth2.Config
-}
-
-var AppConfig Config
-
+// GithubConfig returns the oauth2 configuration for GitHub authentication.
 func GithubConfig() oauth2.Config {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Some error occured. Err: %s", err)
-	}
+    // Load environment variables from .env file
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatalf("Some error occured. Err: %s", err)
+    }
 
-	AppConfig.GithubLoginConfig = oauth2.Config{
-		RedirectURL:  "http://localhost:8080/github_callback",
-		ClientID:     os.Getenv("GITHUB_CLIENT_ID"),
-		ClientSecret: os.Getenv("GITHUB_CLIENT_SECRET"),
-		Scopes:       []string{"user"},
-		Endpoint:     github.Endpoint,
-	}
-
-	return AppConfig.GithubLoginConfig
+    // Return the oauth2 configuration for GitHub
+    return oauth2.Config{
+        // RedirectURL is the URL to redirect after a successful GitHub authentication
+        RedirectURL:  "http://localhost:8080/github_callback",
+        ClientID:     os.Getenv("GITHUB_CLIENT_ID"),
+        ClientSecret: os.Getenv("GITHUB_CLIENT_SECRET"),
+        // Scopes define the access level the application is requesting
+        Scopes:       []string{"user"},
+        // Endpoint is the GitHub OAuth endpoint
+        Endpoint:     github.Endpoint,
+    }
 }
