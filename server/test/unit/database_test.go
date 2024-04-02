3c0302a0751c3bb27f88e2b93634c2ca1df1a504
Legrand Damien <74177609+Nokomi6@users.noreply.github.com>
2024-04-02 12:14:21 +0000

    test: create unit test for check connection twith database

---
+++ server/test/unit/database_test.go
@@ -0,0 +1,19 @@
+package unit
+
+import (
+	"context"
+	"server/configs"
+	"testing"
+)
+
+func TestConnectToDB(t *testing.T) {
+	client, err := configs.ConnectToDB()
+	if err != nil {
+		t.Fatalf("Error when try to connect to database : %v", err)
+	}
+
+	err = client.Disconnect(context.Background())
+	if err != nil {
+		t.Fatalf("Error during database deconnection : %v", err)
+	}
+}
