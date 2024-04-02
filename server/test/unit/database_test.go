7cb907983c82351db77db3bc43eac3ace836cf8b
Legrand Damien <74177609+Nokomi6@users.noreply.github.com>
2024-04-02 12:47:35 +0000

    ci: update pipeline for launching test with specific env var

--- server/test/unit/database_test.go
+++ server/test/unit/database_test.go
@@ -2,11 +2,21 @@ package unit
 
 import (
 	"context"
+	// "flag"
 	"server/configs"
 	"testing"
 )
 
+// var MONGODB_URI string
+
+// In case of launching test in your computer, you can use the following commented code and update test.sh file with -MONGODB_URI=uri
+// func init() {
+//     flag.StringVar(&MONGODB_URI, "MONGODB_URI", "", "Database mongodb_uri")
+// }
+
 func TestConnectToDB(t *testing.T) {
+	// t.Setenv("MONGODB_URI", MONGODB_URI)
+
 	client, err := configs.ConnectToDB()
 	if err != nil {
 		t.Fatalf("Error when try to connect to database : %v", err)
