77c1af65819e083139b139715d7d9a1e0441d145
Legrand Damien <74177609+Nokomi6@users.noreply.github.com>
2024-04-02 12:47:35 +0000

    ci: update pipeline for launching test with specific env var

--- server/configs/database.go
+++ server/configs/database.go
@@ -3,35 +3,56 @@ package configs
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-
+// GetCollection returns a MongoDB collection with the specified name.
 func GetCollection(collectionName string) (*mongo.Collection, error) {
-	client, _ := ConnectToDB()
+	// Connect to the MongoDB database
+	client, err := ConnectToDB()
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %v", err)
+	}
 
 	if client == nil {
 		return nil, fmt.Errorf("client is nil")
 	}
 
+	// Return the MongoDB collection
 	return client.Database("t-web-800").Collection(collectionName), nil
 }
 
-
+// ConnectToDB connects to the MongoDB database and returns a client.
 func ConnectToDB() (*mongo.Client, error) {
-	ctx := context.Background()
-
-	// Utilisation de SetServerAPIOptions() pour définir la version de l'API Stable sur le client
-	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI("mongodb+srv://nokomi:QgPHVAOvC1yIre8s@cluster0.yk2udcw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").SetServerAPIOptions(serverAPI)
-
-	// Création d'un nouveau client et connexion au serveur
-	client, err := mongo.Connect(ctx, opts)
-	if err != nil {
-		return nil, err
+	// Load environment variables from .env file
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file. Using environment variables")
 	}
 
+	// Get the MongoDB URI from the environment variables
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("Please set MONGODB_URI")
+	}
+
+	// Create a background context
+	ctx := context.Background()
+
+	// Set the server API options
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+
+	// Connect to the MongoDB server
+	client, err := mongo.Connect(ctx, opts)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to MongoDB server: %v", err)
+	}
+
+	// Return the MongoDB client
 	return client, nil
 }
