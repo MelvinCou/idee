e194ead56bd5546a73523e438b21448be911d7e4
Legrand Damien <74177609+Nokomi6@users.noreply.github.com>
2024-04-04 19:15:50 +0000

    feat: create singleton for database connection

--- server/configs/database.go
+++ server/configs/database.go
@@ -5,10 +5,17 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	clientInstance     *mongo.Client
+	clientInstanceErr  error
+	clientInstanceOnce sync.Once
 )
 
 // GetCollection returns a MongoDB collection with the specified name.
@@ -29,30 +36,34 @@ func GetCollection(collectionName string) (*mongo.Collection, error) {
 
 // ConnectToDB connects to the MongoDB database and returns a client.
 func ConnectToDB() (*mongo.Client, error) {
-	// Load environment variables from .env file
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file. Using environment variables")
-	}
+	clientInstanceOnce.Do(func() {
+		// Load environment variables from .env file
+		if err := godotenv.Load(); err != nil {
+			log.Println("No .env file. Using environment variables")
+		}
 
-	// Get the MongoDB URI from the environment variables
-	uri := os.Getenv("MONGODB_URI")
-	if uri == "" {
-		log.Fatal("Please set MONGODB_URI")
-	}
+		// Get the MongoDB URI from the environment variables
+		uri := os.Getenv("MONGODB_URI")
+		if uri == "" {
+			clientInstanceErr = fmt.Errorf("Please set MONGODB_URI")
+			return
+		}
+		// Create a background context
+		ctx := context.Background()
 
-	// Create a background context
-	ctx := context.Background()
+		// Set the server API options
+		serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+		opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
 
-	// Set the server API options
-	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+		// Connect to the MongoDB server
+		client, err := mongo.Connect(ctx, opts)
+		if err != nil {
+			clientInstanceErr = fmt.Errorf("failed to connect to MongoDB server: %v", err)
+			return
+		}
 
-	// Connect to the MongoDB server
-	client, err := mongo.Connect(ctx, opts)
-	if err != nil {
-		return nil, fmt.Errorf("failed to connect to MongoDB server: %v", err)
-	}
+		clientInstance = client
+	})
 
-	// Return the MongoDB client
-	return client, nil
+	return clientInstance, clientInstanceErr
 }
