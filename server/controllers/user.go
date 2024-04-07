fa58397d395c12016e40cd051676925cb5d8743f
Legrand Damien <74177609+Nokomi6@users.noreply.github.com>
2024-04-07 09:49:26 +0000

    Pending changes exported from your codespace

--- server/controllers/user.go
+++ server/controllers/user.go
@@ -1,57 +1,18 @@
 package controllers
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"net/http"
+	"server/models"
+	"server/services"
 	"server/configs"
-
-	"github.com/gin-gonic/gin"
-	// "go.mongodb.org/mongo-driver/bson"
 )
 
-func CreateUserHandler(ctx *gin.Context) {
-	// var user User
+var CreateUserService = services.CreateUser
 
-	// err := json.NewDecoder(r.Body).Decode(&user)
-	// if err != nil {
-	// 	http.Error(w, "Bad request", http.StatusBadRequest)
-	// 	return
-	// }
+func CreateUserHandler(user models.User) {
+	userCollection, _ := configs.GetCollection("users")
 
-	err := createUser(User{
-		Name:  "Jean",
-		Email: "jean@epitech.eu",
-	})
-
-	if err != nil {
-		fmt.Print("HAHAHAHHA")
-		// http.Error(w, "Failed to add user", http.StatusInternalServerError)
-		return
-	}
-
-	ctx.JSON(http.StatusCreated, Response{Message: "user created"})
-}
-
-func createUser(user User) error {
-	collection, _ := configs.GetCollection("users")
-
-	// Insertion d'un document
-	insertResult, err := collection.InsertOne(context.TODO(), user)
-	if err != nil {
-		log.Fatalf("Erreur lors de l'insertion du document : %v", err)
-	}
-
-	// Affichage de l'ID du document inséré
-	fmt.Printf("Document inséré avec l'ID : %v\n", insertResult.InsertedID)
-
-	return nil
-}
-
-type User struct {
-	Name  string `bson:"name"`
-	Email string `bson:"email"`
+	// Appeler la fonction CreateUser
+	CreateUserService(userCollection, user)
 }
 
 type Response struct {
