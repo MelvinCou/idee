34ed532c2e1cd8f9cc495089ae25842947cd0c83
ressekkaki <148575476+ressekkaki@users.noreply.github.com>
2024-04-08 15:11:02 +0000

    fix(git) : adjust commits names

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,23 +2,9 @@
 import { RouterLink, RouterView } from "vue-router";
 import { ref } from "vue";
 
-const api = new Ping({
-  baseUrl: import.meta.env.VITE_API_URL,
-});
-const ping = ref();
-
-const getPing = async () => {
-  ping.value = (await api.pingList()).data;
-};
-
-getPing();
-
 const githubOAuth2 = async () => {
   window.location.href = `${import.meta.env.VITE_BASE_URL}/github_login`;
 };
-import LandingPage from "./views/LandingPage.vue";
-import { Ping } from "./api/Ping";
-import { ref } from "vue";
 </script>
 
 <template>
