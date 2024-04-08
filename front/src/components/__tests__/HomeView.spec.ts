7114eb403603d50fe5b5e9f15d68d682d174b179
MansourWolou <woloumansour@gmail.com>
2024-04-08 13:37:21 +0000

    refactor(front devcontainer server/docs): Formating
    feat(vue): creating landing page and add to layout
    
    fix(vue): fix end to end test for ci
    
    fix(vue): correct warnings for PR
    
    hotfix(test) : fix test for ci
    
    feat(vue): creating landing page and add to layout
    
    feat(vue): creating landing page and add to layout

--- front/src/components/__tests__/HomeView.spec.ts
+++ front/src/components/__tests__/HomeView.spec.ts
@@ -1,11 +1,13 @@
 import { describe, it, expect } from "vitest";
 
 import { shallowMount } from "@vue/test-utils";
-import HomeView from "./../../views/HomeView.vue";
+import LandingPage from "./../../views/LandingPage.vue";
 
 describe("HelloWorld", () => {
   it("renders properly", () => {
-    const wrapper = shallowMount(HomeView);
-    expect(wrapper.find("h1").text()).toContain("RECHERCHE!");
+    const wrapper = shallowMount(LandingPage);
+    expect(wrapper.find("h1").text()).toContain(
+      "Lancez-vous dans un voyage et planifiez votre itinéraire",
+    );
   });
 });
