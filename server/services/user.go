5963dfac79aa6e431986daaea3cb9c39c33816e5
Legrand Damien <74177609+Nokomi6@users.noreply.github.com>
2024-04-08 09:54:41 +0000

    feat: create function for create user and check if test works

--- server/services/user.go
+++ server/services/user.go
@@ -2,23 +2,38 @@ package services
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	"server/models"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+// CreateUser inserts a new user into the database collection.
 func CreateUser(collection *mongo.Collection, user models.User) (*mongo.InsertOneResult, error) {
 	insertResult, err := collection.InsertOne(context.TODO(), user)
 
-	fmt.Print("User inserted")
-
 	if err != nil {
-		log.Fatalf("Error during user insertion : %v", err)
+		log.Fatalf("Error during user insertion: %v", err)
 		return nil, err
 	}
 
 	return insertResult, nil
+}
+
+// FindUser searches for a user in the database collection by nodeID.
+func FindUser(collection *mongo.Collection, nodeID string) *models.User {
+	filter := bson.M{"node_id": nodeID}
+
+	var user models.User
+	err := collection.FindOne(context.Background(), filter).Decode(&user)
+
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil
+		}
+	}
+
+	return &user
 }
