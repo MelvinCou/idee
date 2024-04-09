6930d9fcc92d042bd1ff5865571b71de6b42e15a
ressekkaki <148575476+ressekkaki@users.noreply.github.com>
2024-04-09 11:48:46 +0000

    fix(vue): fix end to end test for ci

--- front/cypress/e2e/example.cy.ts
+++ front/cypress/e2e/example.cy.ts
@@ -3,6 +3,6 @@
 describe("My First Test", () => {
   it("visits the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "RECHERCHE!");
+    cy.contains("h1", "Embark on a Journey & plan your trip");
   });
 });
