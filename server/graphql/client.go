e3a6a260e71467f3bce820d9a2d838e4018ad4da
MelvinCou <113338357+MelvinCou@users.noreply.github.com>
2024-04-09 11:08:36 +0000

    feat(server): GraphQL client is now a singleton

--- server/graphql/client.go
+++ server/graphql/client.go
@@ -1,9 +1,27 @@
 package graphql
 
-import "github.com/Khan/genqlient/graphql"
+import (
+	"log"
+	"net/http"
+	"os"
 
-var Client graphql.Client
+	"github.com/Khan/genqlient/graphql"
+)
+
+var client graphql.Client
 
 type Error struct {
 	Error string `json:"error" example:"string"`
+}
+
+func GetClient() graphql.Client {
+	if client == nil {
+		datatourisme := os.Getenv("DATATOURISME_URI")
+		if datatourisme == "" {
+			log.Fatal("Please set DATATOURISME_URI")
+		}
+		client = graphql.NewClient(datatourisme, &http.Client{})
+	}
+
+	return client
 }
