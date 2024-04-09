e9b6a98495e70d6782ab1ee16709c3dd28419af9
MelvinCou <113338357+MelvinCou@users.noreply.github.com>
2024-04-09 11:05:10 +0000

    refactor(server): Move gin router to its own package

---
+++ server/router/router.go
@@ -0,0 +1,42 @@
+package router
+
+import (
+	"net/http"
+	"server/controllers"
+
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+
+	// Middleware CORS
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusOK)
+			return
+		}
+
+		c.Next()
+	})
+
+	api := r.Group("/api")
+
+	{
+		api.GET("/total", controllers.Total)
+	}
+
+	r.GET("/github_login", controllers.GithubLogin)
+	r.GET("/github_callback", controllers.GithubCallback)
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	return r
+}
