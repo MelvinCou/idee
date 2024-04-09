34001e8d6b050d4613589ca5d5979b503979dbd8
MelvinCou <113338357+MelvinCou@users.noreply.github.com>
2024-04-09 12:50:14 +0000

    feat(server): Implemente drink

--- server/controllers/drink.go
+++ server/controllers/drink.go
@@ -1,9 +1,40 @@
 package controllers
 
 import (
+	"net/http"
+	"server/graphql"
+	"server/utils"
+
 	"github.com/gin-gonic/gin"
 )
 
-func Drink(ctx *gin.Context) {
+type DrinkParams struct {
+	City string `query:"city" binding:"required" example:"Paris"`
+	Page int    `query:"page" binding:"required,gte=1" example:"1"`
+}
 
+// Drink godoc
+//
+//	@Summary		Total
+//	@Description	Get available drinks (bars, bistros) in a city
+//	@Tags			graphql
+//	@Accept			json
+//	@Produce		json
+//	@Param			params	query		controllers.DrinkParams	true	"City and page parameters"
+//	@Success		200		{object}	graphql.GetDrinksResponse
+//	@Failure		500		{object}	graphql.Error
+//	@Router			/drink [get]
+func Drink(ctx *gin.Context) {
+	var p DrinkParams
+	if err := ctx.ShouldBindQuery(&p); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	r, err := graphql.GetDrinks(ctx, graphql.GetClient(), p.City, utils.GetFromForGraphQL(p.Page), 20)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, ctx.Error(err))
+	} else {
+		ctx.JSON(http.StatusOK, r)
+	}
 }
