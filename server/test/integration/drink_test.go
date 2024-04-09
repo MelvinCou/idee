ff5840eea6b803b05b0ae10c88914d87f756a37e
MelvinCou <113338357+MelvinCou@users.noreply.github.com>
2024-04-09 12:48:34 +0000

    test(server): Improve drink unit test

--- server/test/integration/drink_test.go
+++ server/test/integration/drink_test.go
@@ -3,9 +3,11 @@ package integration
 import (
 	"net/http"
 	"net/http/httptest"
+	"server/graphql"
 	"server/router"
 	"testing"
 
+	"github.com/gin-gonic/gin/binding"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,12 +15,29 @@ const (
 	endpoint = "/api/drink"
 )
 
-func TestGetDrinkFromParis(t *testing.T) {
+func TestGetDrinkBadRequest(t *testing.T) {
 	router := router.SetupRouter()
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", endpoint, nil)
+	req, _ := http.NewRequest("GET", endpoint, http.NoBody)
 	router.ServeHTTP(w, req)
 
-	assert.Equal(t, 200, w.Code)
-	//assert.Equal(t, "", w.Body.)
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
+
+func TestGetDrinkFromParis(t *testing.T) {
+	var resp graphql.GetDrinksResponse
+	router := router.SetupRouter()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", endpoint+"?City=Paris&Page=1", http.NoBody)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, w.Code, http.StatusOK)
+	if err := binding.JSON.BindBody(w.Body.Bytes(), &resp); err != nil {
+		assert.Fail(t, err.Error())
+	}
+
+	assert.GreaterOrEqual(t, resp.Poi.Total, 1)
+	for _, result := range resp.Poi.Results {
+		assert.GreaterOrEqual(t, len(result.Rdf_type), 1)
+	}
 }
