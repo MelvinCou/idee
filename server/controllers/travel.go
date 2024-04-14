e08a8dce27d2ef851fa3d8b67ed187c53c972e28
Legrand Damien <74177609+Nokomi6@users.noreply.github.com>
2024-04-14 16:10:56 +0000

    feat: create GetTravel controllers functions for make test right

---
+++ server/controllers/travel.go
@@ -0,0 +1,47 @@
+package controllers
+
+import (
+	"net/http"
+	"server/graphql"
+	"server/utils"
+
+	"github.com/gin-gonic/gin"
+)
+
+type TravelParams struct {
+	City string `query:"city" binding:"required" example:"Paris"`
+	Page int    `query:"page" binding:"required,gte=1" example:"1"`
+}
+
+// Travel godoc
+//
+//	@Summary		Travel
+//	@Description	Get available travel (bus station, train station, ...) in a city
+//	@Tags			graphql
+//	@Accept			json
+//	@Produce		json
+//	@Param			params	query		controllers.TravelParams	true	"City and page parameters"
+//	@Success		200		{object}	graphql.GetTravelsResponse
+//	@Failure		400		{object}	graphql.Error	"Bad Request"
+//	@Failure		500		{object}	graphql.Error	"Internal Server Error"
+//	@Router			/drink [get]
+func Travel(ctx *gin.Context) {
+	var p TravelParams
+	if err := ctx.ShouldBindQuery(&p); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	from, err := utils.GetFromForGraphQL(p.Page)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	r, err := graphql.GetTravel(ctx, graphql.GetClient(), p.City, from, 20)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, ctx.Error(err))
+	} else {
+		ctx.JSON(http.StatusOK, r)
+	}
+}
