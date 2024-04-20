495d6ab639a8b842adb17fa2cef4a4e764e1b953
MansourWolou <woloumansour@gmail.com>
2024-04-20 17:16:09 +0000

    feat(front): rebase and added itinary function
    The function is not colled yet on button pressed

--- front/src/components/MapComponent.vue
+++ front/src/components/MapComponent.vue
@@ -1,65 +1,94 @@
 <script setup lang="ts">
 import mapboxgl from "mapbox-gl";
-import { onMounted, ref, watch } from "vue";
+import type * as Geojson from "geojson";
+import { onMounted, reactive, ref, watch } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import { useMapPointsStore } from "@/stores/mapPoints";
 
-const router = useRouter();
-const route = useRoute();
-const MAPBOX_ACCESS_TOKEN = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
-mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
-
-/**
- * Represents the properties required for the map.
- */
-interface MapProps {
-  center: [number, number];
-  maxBounds: [[number, number], [number, number]];
+export interface DirectionResponse {
+  waypoints: Waypoint[];
+  routes: Route[];
+  code: string;
 }
 
-const map = ref<mapboxgl.Map | null>(null);
+export interface Route {
+  legs: Leg[];
+  weight_name: string;
+  geometry: Geometry;
+  weight: number;
+  distance: number;
+  duration: number;
+}
 
-/**
- * Default map properties.
- */
-const mapProps: MapProps = {
-  center: [2.3522, 48.8566],
-  maxBounds: [
-    [-180, -85],
-    [180, 85],
-  ],
+export interface Geometry {
+  coordinates: Array<number[]>;
+  type: string;
+}
+
+export interface Leg {
+  steps: any[];
+  weight: number;
+  distance: number;
+  summary: string;
+  duration: number;
+}
+
+export interface Waypoint {
+  location: number[];
+  name: string;
+}
+
+// interface MapBoxDirectionsResponse {
+//   code: string;
+//   uuid: string;
+//   waypoints: Waypoint[];
+//   routes: Route[];
+// }
+const MAPBOX_ACCESS_TOKEN = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN; // Access token from environment variable
+const MAP_STYLE = "mapbox://styles/mapbox/streets-v12";
+const mapProps: mapboxgl.MapboxOptions = {
+  // Define map properties
+  container: "mapContainer",
+  style: MAP_STYLE,
+  center: [-1.553621, 47.218372], // starting position [lng, lat]
+  zoom: 13, // starting zoom
 };
+const start: number[] = [-1.55818, 47.20484];
+const end: number[] = [-1.5667129, 47.2106158];
+
+const router = useRouter();
+const route = useRoute();
+
+mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
+
+interface MapState {
+  map: mapboxgl.Map | null; // Interface for the map state
+}
+//  let map: ref<mapboxgl.Map | null>(null); // Reactive ref for the map instance
+const map = ref<MapState>({
+  map: null, // Initialize the map as null initially
+});
 
 /**
  * Initializes the map with default properties and controls.
  */
 const initializeMap = () => {
   if (map.value) {
-    map.value.remove();
+    map.value.map?.remove();
   }
 
-  map.value = new mapboxgl.Map({
-    container: "mapContainer",
-    style: "mapbox://styles/mapbox/streets-v11",
-    ...mapProps,
+  map.value.map = new mapboxgl.Map(mapProps);
+  map.value.map.addControl(new mapboxgl.NavigationControl(), "top-right");
+  new mapboxgl.Marker().setLngLat([103.811279, 1.345399]).addTo(map.value.map);
+  const geolocate = new mapboxgl.GeolocateControl({
+    positionOptions: {
+      enableHighAccuracy: true,
+    },
+    trackUserLocation: true,
   });
 
-  map.value.addControl(new mapboxgl.NavigationControl(), "top-right");
-  map.value.addControl(
-    new mapboxgl.GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: true,
-      },
-      trackUserLocation: true,
-    }),
-    "top-right",
-  );
+  map.value.map.addControl(geolocate, "top-right");
 };
-
-/**
- * Fetches city details and updates the map view.
- * @param cityId - The city ID to fetch details for.
- */
 const fetchCityDetails = async (cityId: string) => {
   try {
     const response = await fetch(
@@ -68,11 +97,12 @@ const fetchCityDetails = async (cityId: string) => {
     const data = await response.json();
     if (data.features && data.features.length > 0) {
       const coordinates = data.features[0].geometry.coordinates as [number, number];
-      map.value?.flyTo({
+      map.value?.map?.flyTo({
         center: coordinates,
         essential: true,
         zoom: 10,
       });
+      new mapboxgl.Marker().setLngLat(coordinates).addTo(map.value.map!);
     } else {
       console.error("No features found in the data");
     }
@@ -80,12 +110,122 @@ const fetchCityDetails = async (cityId: string) => {
     console.error("Error fetching city details:", error);
   }
 };
+async function fetchDirections(
+  origin: number[],
+  destination: number[] | undefined,
+  profile: string,
+  token: string,
+  map: mapboxgl.Map | null,
+): Promise<DirectionResponse> {
+  const accessToken = token;
+  // Use destination if provided, otherwise default to origin
+  const finalDestination = destination ?? origin;
+
+  const url = `https://api.mapbox.com/directions/v5/mapbox/${profile}/${origin[0]},${origin[1]};${finalDestination[0]},${finalDestination[1]}?geometries=geojson&access_token=${accessToken}&overview=full`;
+
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  const res = response.json() as Promise<DirectionResponse>;
+  const data = (await res).routes[0];
+  const route = data.geometry.coordinates;
+  const geojson: Geojson.Feature = {
+    type: "Feature",
+    properties: {},
+    geometry: {
+      type: "LineString",
+      coordinates: route,
+    },
+  };
+  // if the route already exists on the map, we'll reset it using setData
+  if (map?.getSource("route")) {
+    const geojsonsource: mapboxgl.AnySourceImpl = map.getSource("route");
+    if (geojsonsource.type == "geojson") {
+      geojsonsource.setData(geojson);
+    }
+  }
+  // otherwise, we'll make a new request
+  else {
+    map?.addLayer({
+      id: "route",
+      type: "line",
+      source: {
+        type: "geojson",
+        data: geojson,
+      },
+      layout: {
+        "line-join": "round",
+        "line-cap": "round",
+      },
+      paint: {
+        "line-color": "#3887be",
+        "line-width": 5,
+        "line-opacity": 0.75,
+      },
+    });
+    map?.addLayer({
+      id: "STARTpoint",
+      type: "circle",
+      source: {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: [
+            {
+              type: "Feature",
+              properties: {},
+              geometry: {
+                type: "Point",
+                coordinates: start,
+              },
+            },
+          ],
+        },
+      },
+      paint: {
+        "circle-radius": 10,
+        "circle-color": "#3887be",
+      },
+    });
+    map?.addLayer({
+      id: "ENDpoint",
+      type: "circle",
+      source: {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: [
+            {
+              type: "Feature",
+              properties: {},
+              geometry: {
+                type: "Point",
+                coordinates: end,
+              },
+            },
+          ],
+        },
+      },
+      paint: {
+        "circle-radius": 10,
+        "circle-color": "#ff0000",
+      },
+    });
+  }
+  // add turn instructions here at the end
+
+  return response.json() as Promise<DirectionResponse>;
+}
 
 onMounted(() => {
   initializeMap();
   if (route.params.cityId) {
     fetchCityDetails(route.params.cityId as string);
   }
+  fetchDirections(start, end, "cycling", MAPBOX_ACCESS_TOKEN, map.value.map)
+    .then((data) => console.log(data))
+    .catch((error) => console.error("There was a problem with your fetch operation: ", error));
 });
 
 watch(router.currentRoute, () => {
