2c1aff46b7115b3f3b249ae1de33d7c7cfdb5594
Legrand Damien <74177609+Nokomi6@users.noreply.github.com>
2024-04-21 12:30:43 +0000

    fix: change deconnexion by déconnexion and add some style if no images

--- front/src/components/CarouselsComponent.vue
+++ front/src/components/CarouselsComponent.vue
@@ -6,23 +6,18 @@ const props = defineProps<{
 </script>
 
 <template>
-  <v-carousel v-if="images && images?.length >= 1" :class="props.mainLayout && 'main-layout-class'">
-    <v-carousel-item v-for="image in props.images" :key="image" :src="image" cover />
+  <v-carousel :class="props.mainLayout && 'main-layout-class'">
+    <template v-if="props.images && props.images.length >= 1">
+      <v-carousel-item v-for="image in props.images" :key="image" :src="image" cover />
+    </template>
+    <v-carousel-item
+      v-else
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2048px-No_image_available.svg.png" />
   </v-carousel>
-  <img
-    v-else
-    class="no-image"
-    cover
-    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2048px-No_image_available.svg.png" />
 </template>
 
 <style scoped>
 .main-layout-class {
   height: 20% !important;
-}
-
-.no-image {
-  height: 20% !important;
-  width: -webkit-fill-available;
 }
 </style>
