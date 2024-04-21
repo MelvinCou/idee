8e6e17a42bd756f5f1f2ed10f06a3e17a8034358
Legrand Damien <74177609+Nokomi6@users.noreply.github.com>
2024-04-21 11:46:46 +0000

    docs: add some jsDoc and delete some unused css class

--- front/src/views/LandingPage.vue
+++ front/src/views/LandingPage.vue
@@ -1,11 +1,40 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import type { DatePickerInstance } from "@vuepic/vue-datepicker";
+import { useRouter } from "vue-router";
+
+// Initialize useRouter
+const router = useRouter();
+// Reactive reference for date
+const date = ref<Date[]>([]);
+// Reference for datepicker instance
+const datepicker = ref<DatePickerInstance>(null);
+
+// Set default date range on mount
+onMounted(() => {
+  const startDate = new Date();
+  const endDate = new Date(new Date().setDate(startDate.getDate() + 7));
+  date.value = [startDate, endDate];
+});
+
+// Process user inputs and navigate to main page
+const processInputs = () => {
+  router.push({
+    name: "MainPage",
+  });
+};
+</script>
+
 <template>
   <div class="LandingPage">
     <div class="text-content">
+      <!-- Landing page content -->
       <h1>Lancez-vous dans un voyage et planifiez votre itinéraire</h1>
       <p class="subheading">
         Découvrez l'art du voyage grâce à cette application conviviale qui vous permet de planifier
         vos itinéraires et de choisir des bars, restaurants et points d'intérêt.
       </p>
+      <!-- Date picker component -->
       <div class="date-picker-container">
         <VueDatePicker
           v-model="date"
@@ -18,39 +47,13 @@
           prevent-min-max-navigation
           ref="datepicker" />
       </div>
-      <v-btn @click="processInputs" large color="red" dark class="mb-2"> Start planning </v-btn>
+      <!-- Start adventure button -->
+      <v-btn @click="processInputs" large color="red" dark class="mb-2"
+        >Commencez votre aventure</v-btn
+      >
     </div>
   </div>
 </template>
-
-<script setup lang="ts">
-import { ref, onMounted } from "vue";
-import type { DatePickerInstance } from "@vuepic/vue-datepicker";
-import { useRouter } from "vue-router";
-
-const router = useRouter();
-const date = ref<Date[]>([]);
-const selectedDestination = ref("");
-
-const datepicker = ref<DatePickerInstance>(null);
-
-onMounted(() => {
-  const startDate = new Date();
-  const endDate = new Date(new Date().setDate(startDate.getDate() + 7));
-  date.value = [startDate, endDate];
-});
-
-const processInputs = () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const formattedDate = date.value.map((d) => d.toISOString().substring(0, 10)).join(" to ");
-  router.push({
-    name: "MainPage",
-    params: { cityId: selectedDestination.value },
-  });
-
-  //TODO: Add logic to process the user inputs
-};
-</script>
 
 <style scoped>
 .LandingPage {
@@ -82,49 +85,16 @@ const processInputs = () => {
   padding-right: 40px;
 }
 
-.search-destination,
 .date-picker-container {
   max-width: 500px;
   margin: 20px auto;
   width: 100%;
 }
 
-.search-destination .v-input__slot {
-  background-color: rgba(255, 255, 255, 10);
-  border-radius: 10px;
-}
-
-.search-destination .v-input__control {
-  background-color: rgba(255, 255, 255, 100);
-}
-
-.search-destination .v-select__selections {
-  color: #000;
-}
-
 .wrap {
   display: flex;
   justify-content: center;
   padding: 20px;
-}
-
-.search-input-container {
-  width: 100%;
-  max-width: 600px;
-}
-
-.search {
-  width: 80%;
-  padding: 10px 40px 10px 15px;
-  font-size: 16px;
-  border: 2px solid #ccc;
-  border-radius: 25px;
-  outline: none;
-  box-sizing: border-box;
-}
-
-.search:focus {
-  border-color: #007bff;
 }
 
 .feather-icon {
